5416fd6868502baf6bdd556efed558af757459d6
skmajumder <skmajumder.cse@gmail.com>
2023-05-20 09:18:12 +0600

    Single toy details complete

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -21,104 +21,112 @@ const Header = () => {
   };
 
   return (
-    <header className="bg-white-900 p-4">
-      <div className="container px-10">
-        <div className="flex items-center justify-between">
-          <div className="w-3/12">
-            <img src={Logo} alt="Logo" className="h-auto md:h-14" />
+    <>
+      <header className="bg-white-900 p-4">
+        <div className="container px-10">
+          <div className="flex items-center justify-between">
+            <div className="w-3/12">
+              <img src={Logo} alt="Logo" className="h-auto md:h-14" />
+            </div>
+            <nav className="w-6/12 hidden md:block">
+              {/* Navigation menu */}
+              <ul className="flex justify-center space-x-6 text-[18px] text-[#02224d] font-bold leading-6">
+                <li>
+                  <Link to={"/"}>Home</Link>
+                </li>
+                <li>
+                  <Link to={"/all-toys"}>All Toys</Link>
+                </li>
+                {user && (
+                  <li>
+                    <Link to={"/"}>My Toys</Link>
+                  </li>
+                )}
+                {user && (
+                  <li>
+                    <Link to={"/add-toy"}>Add A Toy</Link>
+                  </li>
+                )}
+                <li>
+                  <Link to={"/"}>Blog</Link>
+                </li>
+              </ul>
+            </nav>
+            <div className="w-3/12 flex justify-start md:justify-end gap-0 md:gap-3">
+              {user ? (
+                <>
+                  <div className="tooltip" data-tip={user?.displayName}>
+                    <img
+                      src={defaultUserAvatar}
+                      alt="User Avatar"
+                      className="h-8 rounded-full"
+                    />
+                  </div>
+                  <button
+                    onClick={logout}
+                    className="btn btn-sm hidden md:block"
+                  >
+                    Logout
+                  </button>
+                </>
+              ) : (
+                <>
+                  <button
+                    onClick={handleRedirectLogin}
+                    className="btn btn-sm hidden md:block"
+                  >
+                    Login
+                  </button>
+                </>
+              )}
+            </div>
+            <button
+              className="block md:hidden"
+              onClick={handleToggleMobileMenu}
+              aria-label="Toggle mobile menu"
+            >
+              <svg
+                className="w-6 h-6 fill-current text-[#02224d]"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M3 5h18a1 1 0 0 1 0 2H3a1 1 0 0 1 0-2zm0 6h18a1 1 0 0 1 0 2H3a1 1 0 1 1 0-2zm0 6h18a1 1 0 0 1 0 2H3a1 1 0 0 1 0-2z"
+                />
+              </svg>
+            </button>
           </div>
-          <nav className="w-6/12 hidden md:block">
-            {/* Navigation menu */}
-            <ul className="flex justify-center space-x-6 text-[18px] text-[#02224d] font-bold leading-6">
-              <li>
-                <Link to={"/"}>Home</Link>
-              </li>
-              <li>
-                <Link to={"/all-toys"}>All Toys</Link>
-              </li>
-              {user && (
+          {isMobileMenuOpen && (
+            <nav className="mt-2">
+              <ul className="flex flex-col items-center space-y-4 text-[18px] text-[#02224d] font-bold leading-6">
+                <li>
+                  <Link to={"/"}>Home</Link>
+                </li>
+                <li>
+                  <Link to={"/"}>All Toys</Link>
+                </li>
                 <li>
                   <Link to={"/"}>My Toys</Link>
                 </li>
-              )}
-              {user && (
                 <li>
-                  <Link to={"/add-toy"}>Add A Toy</Link>
+                  <Link to={"/"}>Add A Toy</Link>
                 </li>
-              )}
-              <li>
-                <Link to={"/"}>Blog</Link>
-              </li>
-            </ul>
-          </nav>
-          <div className="w-3/12 flex justify-start md:justify-end gap-0 md:gap-3">
-            {user ? (
-              <>
-                <div className="tooltip" data-tip={user?.displayName}>
-                  <img
-                    src={defaultUserAvatar}
-                    alt="User Avatar"
-                    className="h-8 rounded-full"
-                  />
-                </div>
-                <button onClick={logout} className="btn btn-sm hidden md:block">
-                  Logout
-                </button>
-              </>
-            ) : (
-              <>
-                <button
-                  onClick={handleRedirectLogin}
-                  className="btn btn-sm hidden md:block"
-                >
-                  Login
-                </button>
-              </>
-            )}
-          </div>
-          <button
-            className="block md:hidden"
-            onClick={handleToggleMobileMenu}
-            aria-label="Toggle mobile menu"
-          >
-            <svg
-              className="w-6 h-6 fill-current text-[#02224d]"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-            >
-              <path
-                fillRule="evenodd"
-                d="M3 5h18a1 1 0 0 1 0 2H3a1 1 0 0 1 0-2zm0 6h18a1 1 0 0 1 0 2H3a1 1 0 1 1 0-2zm0 6h18a1 1 0 0 1 0 2H3a1 1 0 0 1 0-2z"
-              />
-            </svg>
-          </button>
+                <li>
+                  <Link to={"/"}>Blog</Link>
+                </li>
+                <li>
+                  <button className="btn btn-sm md:hidden">Logout</button>
+                </li>
+              </ul>
+            </nav>
+          )}
         </div>
-        {isMobileMenuOpen && (
-          <nav className="mt-2">
-            <ul className="flex flex-col items-center space-y-4 text-[18px] text-[#02224d] font-bold leading-6">
-              <li>
-                <Link to={"/"}>Home</Link>
-              </li>
-              <li>
-                <Link to={"/"}>All Toys</Link>
-              </li>
-              <li>
-                <Link to={"/"}>My Toys</Link>
-              </li>
-              <li>
-                <Link to={"/"}>Add A Toy</Link>
-              </li>
-              <li>
-                <Link to={"/"}>Blog</Link>
-              </li>
-              <li>
-                <button className="btn btn-sm md:hidden">Logout</button>
-              </li>
-            </ul>
-          </nav>
-        )}
+      </header>
+      <div className="flex justify-center">
+        <div className="w-[95%] h-[.5px] bg-gradient-to-r from-[rgba(2,0,36,1)] via-[rgba(9,9,121,0.9866071428571429)] to-[rgba(0,212,255,1)]"></div>
       </div>
-    </header>
+    </>
   );
 };
 
--- src/pages/AllToys/SingleToy.jsx
+++ src/pages/AllToys/SingleToy.jsx
@@ -1,13 +1,107 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { Rating } from "@smastrom/react-rating";
+import "@smastrom/react-rating/style.css";
 
 const SingleToy = () => {
   const { id: toyID } = useParams();
+  const [toyDetails, setToyDetails] = useState([]);
+
+  useEffect(() => {
+    fetch(`http://localhost:3000/toy/${toyID}`)
+      .then((response) => response.json())
+      .then((data) => setToyDetails(data));
+  }, []);
+
+  const {
+    _id: id,
+    pictureUrl,
+    name,
+    sellerName,
+    sellerEmail,
+    price,
+    rating,
+    description,
+    subCategory,
+    availableQuantity,
+  } = toyDetails || {};
+
+  console.log(subCategory);
 
   return (
-    <div>
-      <h2>Single Toy {toyID}</h2>
-    </div>
+    <>
+      <section className="section-single-toy">
+        <div className="container mx-auto py-12 px-10">
+          <div className="flex justify-between items-start mb-10">
+            <div className="w-1/2 text-center">
+              <img className="w-[80%]" src={pictureUrl} alt={name} />
+            </div>
+            <div className="w-1/2 space-y-4">
+              <h1 className="text-2xl font-bold text-[#02224d]">{name}</h1>
+              <div className="w-full my-3 h-px bg-gradient-to-r from-purple-500 to-02224d"></div>
+              <div className="flex justify-start items-center gap-2">
+                <Rating style={{ maxWidth: 80 }} value={rating} readOnly />{" "}
+                <span>({rating}) Rating</span>
+              </div>
+              <p className="text-4xl font-bold text-[#02224d]">
+                Price: ${price}
+              </p>
+              <p>
+                <span className="text-lg text-[#02224d] font-semibold">
+                  Seller:
+                </span>{" "}
+                {sellerName}
+              </p>
+              <p>
+                <span className="text-lg text-[#02224d] font-semibold">
+                  Seller Email:
+                </span>{" "}
+                {sellerEmail}
+              </p>
+              <p>
+                <span className="text-lg text-[#02224d] font-semibold">
+                  Available Quantity:
+                </span>{" "}
+                {availableQuantity}
+                {availableQuantity ? (
+                  <>
+                    <span className="badge badge-success bg-green-500 gap-2 text-white ml-3">
+                      In Stock
+                    </span>
+                  </>
+                ) : (
+                  <>
+                    <span className="badge badge-success bg-red-500 gap-2 text-white ml-3">
+                      Stock Out
+                    </span>
+                  </>
+                )}
+              </p>
+              <p>
+                <span className="text-lg text-[#02224d] font-semibold">
+                  Sub-category:
+                </span>{" "}
+                {subCategory &&
+                  subCategory.map((category, index) => (
+                    <span key={index} className="toy-category">
+                      {category.label}
+                    </span>
+                  ))}
+              </p>
+            </div>
+          </div>
+          <div className="toy-details pr-20">
+            <h2 className="text-4xl font-thin text-[#02224d] mb-10">
+              Description
+            </h2>
+            <div className="w-full my-3 h-px bg-gradient-to-r from-purple-500 to-02224d"></div>
+            <p className="text-[#02224db3] text-[15px] leading-[28px] mb-4">
+              {description}
+            </p>
+          </div>
+        </div>
+      </section>
+    </>
   );
 };
 
